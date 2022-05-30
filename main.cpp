3eaf65aaa21e8be199e807da7446ae4b0cc88236
Andrej <106358524+andrejfilkoski@users.noreply.github.com>
2022-05-30 21:50:31 +0200

    Dodaeno definiranje na vektorot.

--- main.cpp
+++ main.cpp
@@ -33,3 +33,25 @@ struct SortPointers
         return *a > *b;
     }
 };
+vector<covidMK*> vcmk; //definiranje na vektorot
+//8zapisi
+vcmk.push_back(new covidMK( "Stip", 7, 12));
+vcmk.push_back(new covidMK( "Tetovo", 1 , 1 ));
+vcmk.push_back(new covidMK( "Bitola", 13, 7));
+vcmk.push_back(new covidMK( "Kavadarci", 3, 4));
+vcmk.push_back(new covidMK( "Ohrid", 1, 10));
+vcmk.push_back(new covidMK( "Debar", 1, 7));
+vcmk.push_back(new covidMK( "Prilep" , 2, 1));
+vcmk.push_back(new covidMK( "Stip" , 7, 5));
+
+for(const auto* c: vcmk)
+
+cout <<c->grad <<" "<< c->novi << " " << c->ozdraveni<< '\n';
+
+cout<<"Sortirani"<<endl;
+sort(vcmk.begin(), vcmk.end(), SortPointers());
+
+for(const auto* c: vcmk)
+
+cout <<c->grad <<" "<< c->novi << " " << c->ozdraveni<< '\n';
+
